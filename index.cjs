40d8d56b22eac8f69bbe9a730e4c67ba168a3e79
Mitchell Wintrow <90mitchwintrow@gmail.com>
2024-12-07 13:10:25 -0700

    Change express server to .cjs file

--- index.cjs
+++ index.cjs
@@ -1,22 +1,22 @@
-import express from "express";
-import * as Http from "http";
-import * as Https from "https";
-import * as Fs from "fs";
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 
 const options = {
-  cert: Fs.readFileSync("/etc/letsencrypt/live/aspirewithalina.com/fullchain.pem"),
-  key: Fs.readFileSync("/etc/letsencrypt/live/aspirewithalina.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/aspirewithalina.com/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/aspirewithalina.com/privkey.pem"),
 };
 const app = express();
 const PORT = 443;
 
 app.use(express.static("dist"));
 
-Https.createServer(options, app).listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Server is running securely on https://aspirewithalina.com:${PORT}`);
 });
 
-Http
+http
   .createServer((req, res) => {
     console.log(`Redirecting to https://${req.headers.host}${req.url}`);
     res.writeHead(301, {
